7b1eac1555b38da6e9399df95b66a0a2d4e3cc1b
Krishna <k84497838@gmail.com>
2026-03-08 00:27:18 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.post(
 )
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://examnotesaiclient-vl8k.onrender.com" || "http://localhost:5173", //
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
