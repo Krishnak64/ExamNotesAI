262fa9f33d5ebb22a81a26ce0aa9c7a6608d9610
krishnaStr <krir@jkhjd>
2026-03-11 22:09:56 +0530

    updated logout

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -28,10 +28,25 @@ export const googleAuth = async (req, res) => {
 }
 
 
+// export const logOut = async (req, res) => {
+//   try {
+//     res.clearCookie("token");
+//     return res.status(200).json({ message: "Logout successfully!" });
+//   } catch (error) {
+//     return res.status(500).json({ message: `Logout error: ${error.message}` });
+//   }
+// };
+
 export const logOut = async (req, res) => {
   try {
-    res.clearCookie("token");
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none"
+    });
+
     return res.status(200).json({ message: "Logout successfully!" });
+
   } catch (error) {
     return res.status(500).json({ message: `Logout error: ${error.message}` });
   }
